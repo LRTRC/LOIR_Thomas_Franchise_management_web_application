5ea2024128874a27be386c8ceb7c4a5479e12703
LRTRC <97665266+LRTRC@users.noreply.github.com>
2022-10-06 12:02:14 +0200

    [refact] back and front directories (#7)
    * mkdir back and front and init nuxt
    
    * ide settings
    
    * nuxt init
    
    Co-authored-by: root <root@DESKTOP-I516SA9.localdomain>

---
+++ front/pages/inspire.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+      <v-btn @click="fetch">
+        click me
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+  data() {
+    return {
+    franchisees: [],
+    }
+  },
+  methods: {
+    async getFranchisees() {
+      const franchisees = await this.$axios.$get("http://localhost:3000/api/franchisees/")
+    },
+    async fetch() {
+      this.franchisees = await fetch(
+        'http://localhost:3000/api/franchisees/'
+      ).then(res => res.json())
+    }
+  }
+}
+</script>
---
+++ front/pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height">
+    <v-row>
+      <v-col cols="10" md="6">
+        <v-card>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
