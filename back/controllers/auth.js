b1a4eb1ac5578d476b0a385fc9d157ee3531d808
root <root@UC102.VERMON.com>
2022-11-08 11:05:11 +0100

    refresh token

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -46,10 +46,13 @@ const login = async (req, res, next) => {
                         // refresh token will be implemented) todo : sets the secret key in a .env file and implement
                         // todo : refresh token
                         user.token = jwt.sign(
-                            {userId: user.id},
-                            "eyJ1c2VySWQiOjExLCJpYXQiOjE2NjM5NDEwOTk",
-                            {expiresIn: '30m'}
+                            {userId: user.id, userRole: user.role}, process.env.ACCESS_TOKEN,
+                            {expiresIn: '5m'}
                         )
+
+                        user.refresh_token = jwt.sign({userId: user.id, userRole: user.role},
+                            process.env.REFRESH_TOKEN,
+                            {expiresIn: '2h'});
 
                         // delete the key value from user constant
                         delete user.passphrase
@@ -70,5 +73,68 @@ const login = async (req, res, next) => {
     }
 };
 
+const refreshToken = async (req, res, next) => {
+    try {
+
+        // get refresh token
+        const refreshToken = req.body.refresh_token.split(' ')[1]
+
+        // if there is one
+        if (refreshToken) {
+
+            // verify refresh token with secret key in .env file
+            const verifiedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN, function (err, decoded) {
+                // if an error occurred, return 401
+                if (err) {
+                    console.log(err)
+                    return res.sendStatus(401)
+                }
+                // if verified
+                if (decoded) {
+                    return decoded
+                }
+            })
+            // check if user exist
+            const userId = verifiedToken.userId
+
+            // build SQL query to find the user with its email
+            const query = "SELECT * FROM users WHERE id=$1;";
+            const value = [userId];
+
+            // send query
+            const data = await pool.query(query, value);
+            // if no result, return 401
+            if (data.rowCount === 0) return res.status(401).send("User or password invalid");
+
+            // if the user exists sets it in the user constant
+            const user = data.rows[0]
+
+            // delete the key value from user constant
+            delete user.passphrase
+
+            // if the user exists
+            if (user && verifiedToken) {
+                user.token = jwt.sign(
+                    {userId: user.id, userRole: user.role}, process.env.ACCESS_TOKEN,
+                    {expiresIn: '5m'}
+                )
+                console.log('token refreshed')
+                // return the user
+                return res.status(200).json({
+                    status: 200,
+                    message: "User:",
+                    data: user
+                })
+            } else {
+                console.log('token refresh rejected')
+                return res.send(401)
+            }
+        }
+    } catch (error) {
+        res.status(401).json({error})
+        console.log(error)
+    }
+}
+
 // export function
-module.exports = {login};
+module.exports = {login, refreshToken};
--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -4,6 +4,12 @@ const jwt = require('jsonwebtoken');
 // import pool object from pg module exported in config.js
 const {pool} = require("../config");
 
+// import dotenv module
+const dotenv = require("dotenv");
+
+// configure dotenv and process.env now has the keys and values defined in .env file
+dotenv.config();
+
 // export a function to verify the token
 module.exports = async (req, res, next) => {
     try {
@@ -12,7 +18,7 @@ module.exports = async (req, res, next) => {
 
         // uses verify method from jwt to decode the token with the key
         // if false an error will occur todo: put secretKey in .env and sets it at the host
-        const decodedToken = jwt.verify(token, 'eyJ1c2VySWQiOjExLCJpYXQiOjE2NjM5NDEwOTk')
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN)
 
         // gets the user's id and gave it to the auth property of the request
         const userId = decodedToken.userId;
