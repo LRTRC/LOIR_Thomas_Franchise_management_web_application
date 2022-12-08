4a17617c7a561e66d073718a468e12d38cdadae1
root <root@UC102.VERMON.com>
2022-12-08 09:14:44 +0100

    tables in UI

--- front/pages/me.vue
+++ front/pages/me.vue
@@ -1,22 +1,118 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" sm="10">
-      <v-card class="card-neumorphism pa-4">
-        {{ user }}
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+
+    <v-row justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="card-neumorphism pa-4">
+          {{ user }}
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="card-neumorphism pa-4">
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-card-title class="titles pb-0">
+                <v-icon class="px-4" color="primary">
+                  {{ icons[1] }}
+                </v-icon>
+                Liste des franchisés
+              </v-card-title>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                id="franchiseesSearchBar"
+                v-model="FranchiseesSearch"
+                clearable
+                :prepend-icon="icons[0]"
+                label="Rechercher"
+                hide-details
+                color="primary"
+              />
+            </v-col>
+          </v-row>
+          <v-data-table
+            id="franchiseesDataTable"
+            class="pa-4 mt-4"
+            :headers="FranchiseesHeaders"
+            :items="franchisees"
+            :items-per-page="franchisees.length"
+            :search="FranchiseesSearch"
+            item-key="id"
+            loading-text="Chargement des données"
+            no-data-text="Aucune donnée"
+            no-results-text="Aucun résultat"
+            hide-default-footer
+            dense
+            multi-sort
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="card-neumorphism pa-4">
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-card-title class="titles pb-0">
+                <v-icon class="px-4" color="primary">
+                  {{ icons[1] }}
+                </v-icon>
+                Liste des structures
+              </v-card-title>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                id="structuresSearchBar"
+                v-model="structuresSearch"
+                clearable
+                :prepend-icon="icons[0]"
+                label="Rechercher"
+                hide-details
+                color="primary"
+              />
+            </v-col>
+          </v-row>
+          <v-data-table
+            id="structuresDataTable"
+            class="pa-4 mt-4"
+            :headers="structuresHeaders"
+            :items="structures"
+            :items-per-page="structures.length"
+            :search="structuresSearch"
+            item-key="id"
+            loading-text="Chargement des données"
+            no-data-text="Aucune donnée"
+            no-results-text="Aucun résultat"
+            hide-default-footer
+            dense
+            multi-sort
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </div>
 </template>
 
 <script>
 import {mapActions, mapGetters} from "vuex";
+import {mdiMagnify, mdiPlaylistCheck} from '@mdi/js';
 
 export default {
   name: "me",
   data() {
     return {
+      icons: [mdiMagnify, mdiPlaylistCheck],
       franchisees: [],
       structures: [],
+      FranchiseesHeaders: [],
+      FranchiseesSearch: [],
+      structuresHeaders: [],
+      structuresSearch: [],
     }
   },
   mounted() {
@@ -39,7 +135,7 @@ export default {
       try {
         const id = user.id
         if (user.id) {
-          const franchisees = await this.$axios.$get(`/api/franchisees_users/${id}`)
+          const franchisees = await this.$axios.$get(`/api/franchisees_users/user/${id}`)
 
           // if response mutate franchisees
           if (franchisees) {
@@ -57,11 +153,11 @@ export default {
       try {
         const id = user.id
         if (user.id) {
-          const franchisees = await this.$axios.$get(`/api/structures_users/${id}`)
+          const structures = await this.$axios.$get(`/api/structures_users/user/${id}`)
 
           // if response mutate franchisees
-          if (franchisees) {
-            return this.franchisees = franchisees
+          if (structures) {
+            return this.structures = structures
           }
         }
 
@@ -72,7 +168,8 @@ export default {
     },
 
     fetData(user) {
-      this.findUserFranchisees(user)
+      this.findUserFranchisees(user);
+      this.findUserStructures(user)
     }
   }
 
@@ -80,5 +177,7 @@ export default {
 </script>
 
 <style scoped>
-
+.titles {
+  font-family: 'Poppins', sans-serif !important;
+}
 </style>
